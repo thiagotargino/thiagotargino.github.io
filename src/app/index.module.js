3570e73166bc00a44c0e5bc436e1ec42aae21939
Thiago Targino <thiagotargino@MacBook-Pro-de-Thiago.local>
2015-11-22 21:26:49 -0300

    Adicionando lib D3

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('thiagotarginoGithubIo', ['ngAnimate', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.utils.masks', 'toastr']);
+    .module('thiagotarginoGithubIo', ['ngAnimate', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.utils.masks', 'nvd3', 'nvd3ChartDirectives', 'toastr']);
 
 })();
--- src/app/relatorio/relatorio.controller.js
+++ src/app/relatorio/relatorio.controller.js
@@ -1,5 +1,5 @@
 (function() {
-  'use strict';
+  // 'use strict';
 
   angular
     .module('thiagotarginoGithubIo')
@@ -12,11 +12,60 @@
 
     vm.listaRelatorios = [];
 
+    vm.data = [
+      {
+        "key": "PE",
+        "y": 9
+     },
+
+     {
+        "key": "BA",
+        "y": 12
+     }
+    ];
+
     var promise = RelatorioServices.getRelatorio();
+
+    vm.dados = [];
+
+    console.log(vm);
 
     promise.then(function(data) {
       vm.listaRelatorios = data.data;
+
+      for (var i = 20 - 1; i >= 0; i--) {
+
+        // dados.push('1');
+        // vm.dados.push( {'key': vm.listaRelatorios[i].uf, 'y': vm.listaRelatorios[i].indice} );
+      }
+
+      // for (var i = 0; i < vm.listaRelatorios.length; i++) {
+      // for (var i = 0; i < 20; i++) {
+      //   console.log( vm.listaRelatorios[i] );
+      // };
+      //
+      // vm.dadosTratados = JSON.stringify(vm.dados);
+      // console.log(vm.dadosTratados);
     });
+
+    vm.xFunction = function() {
+      return function(d) {
+        return d.key;
+        // return d3.time.format('%Y-%m-%d')(new Date(d));
+
+        // var monthNameFormat = d3.time.format("%B");
+        // var yearNameFormat = d3.time.format("%y");
+        // console.log( monthNameFormat(new Date(2014, 0, 1))  );
+      }
+    }
+
+    vm.yFunction = function() {
+      return function(d) {
+        return d.y;
+        // console.log(y);
+      }
+    }
+
   }
 
   function RelatorioServices($http, $q) {
